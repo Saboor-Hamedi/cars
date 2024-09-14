b9a3f8f549a308eb3c9c2fe0525bc0a6fb347089
Saboor-Hamedi <janhamedi49@gmail.com>
2024-09-14 21:18:29 +0700

    "Refactored CSS and Blade templates for card components, vote system, and error display"

--- resources/views/livewire/front-content.blade.php
+++ resources/views/livewire/front-content.blade.php
@@ -2,29 +2,33 @@
     <div class="custom-container">
         @foreach ($cars as $car)
             <div class="card">
-                <div
-                    class="transition-colors duration-300 border-2 border-blue-600 card-icon hover:cursor-pointer hover:border-red-400">
-                    @if ($car->user->profile && $car->user->profile->photo)
-                        <div class="flex justify-start">
-                            <img id="image-preview" src="{{ asset('storage/' . $car->user->profile->photo) }}"
-                                alt="{{ $car->name }} image" class="object-fill custom-circle-image">
-                        </div>
-                    @else
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                            <circle cx="12" cy="7" r="4"></circle>
-                            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                        </svg>
-                    @endif
-                </div>
-                <h2 class="card-title">
-                    <a href="{{ route('show-profile', ['car' => $car->id]) }}">
-                        {!! $car->name !!}
-                    </a>
+                <div class="p-2">
+                    <div
+                        class="transition-colors duration-300 border-2
+                     border-blue-600 card-icon hover:cursor-pointer
+                      hover:border-red-400
+                      py-2">
+                        @if ($car->user->profile && $car->user->profile->photo)
+                            <div class="flex justify-start">
+                                <img id="image-preview" src="{{ asset('storage/' . $car->user->profile->photo) }}"
+                                    alt="{{ $car->name }} image" class="object-fill custom-circle-image">
+                            </div>
+                        @else
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                                <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                            </svg>
+                        @endif
+                    </div>
 
-                </h2>
-                <div class="card-description">
+                    <h2 class="card-title">
+                        <a href="{{ route('show-profile', ['car' => $car->id]) }}">
+                            {!! $car->name !!}
+                        </a>
+
+                    </h2>
                     @php
                         $color = $car->color;
                         if (strpos($color, '#') !== 0) {
@@ -38,8 +42,8 @@
                     <span class="font-bold">Date</span> {{ $car->created_at->format('Y-m-d') }}<br>
                     <span class="font-bold">User:</span> {!! Str::ucfirst($car->user->name) !!}<br>
                     <span class="font-bold">Description:</span> {!! Str::limit($car->description, 30, '...') !!}
-                    @livewire('vote.vote', ['car' => $car])
                 </div>
+                <div class="vote-footer">@livewire('vote.vote', ['car' => $car])</div>
             </div>
         @endforeach
     </div>
